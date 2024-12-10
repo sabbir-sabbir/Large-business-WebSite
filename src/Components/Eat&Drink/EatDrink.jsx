becb51ae4f9562257e8f5ece7381fd569315ed91
sabbir-sabbir <iosabbir7890@gmail.com>
2024-12-10 17:35:30 +0600

    done

--- src/Components/Eat&Drink/EatDrink.jsx
+++ src/Components/Eat&Drink/EatDrink.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import DrinkHero from './DrinkHero'
 import FindPlace from './FindPlace'
+import Resturant from './Resturant'
 
 const EatDrink = () => {
   return (
     <>
     <DrinkHero/>
     <FindPlace/>
+    <Resturant/>
     </>
   )
 }
---
+++ src/Components/Eat&Drink/Resturant.jsx
@@ -0,0 +1,45 @@
+import React, {useState, useEffect} from 'react'
+import {Button} from "@nextui-org/react"
+
+const Resturant = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(()=> {
+        fetch("mydata.json")
+        .then((res)=> {
+            return  res.json()
+        })
+        .then((jsonData)=> {
+            setData(jsonData)
+        })
+
+    } , [])
+
+
+  return (
+   <>
+   <section className='bg-[#f5faff] p-10'>
+    <div className='w-[80%] mx-auto'>
+        <div className='flex justify-between items-center '>
+         <h2 className='font-bold text-2xl md:text-4xl mb-4 mt-4'>Popular resturants in Lagos</h2>
+         <Button variant='outline' className='text-xl text-[#0e8bff]'>See more</Button>
+        </div>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 mt-5'>
+            {data.map((allData)=> (
+
+                <div key={allData.id}>
+                    <img className=' transform hover:scale-110 transition duration-300 ease-in-out  w-[250px] object-cover rounded-lg mb-3' src={allData.img} alt="" />
+                    <h2 className=' text-[#26395C] font-semibold text-sm md:text-lg mb-2 '>{allData.title}</h2>
+                    <p className='text-sm text-[#26395C]'>{allData.desc}</p>
+                    <Button color='primary' className='mt-12'>Reserve Now</Button>
+                </div>
+            ))}
+
+        </div>
+    </div>
+   </section>
+   </>
+  )
+}
+
+export default Resturant
